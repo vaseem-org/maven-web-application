31083337bfdb04a070300face3922fdbbcf0347b
vaseem <vaseem.a@gradatim.co.in>
2023-06-21 13:18:10 +0000

    cm

---
+++ users.java
@@ -0,0 +1,9 @@
+mithun tech
+vaseem akram
+existing file modifing 
+all the files wont move into local repo only existing files which are recently modifies those files only going to move into local repo
+-a option applies onl for that
+
+by mistake you added another file but you didnt modify completely yet so you want to revert or roll back from stagging to working directory
+updated this line for change
+
